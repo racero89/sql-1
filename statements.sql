2ebd95b34953e2d0b423babfc49fead535e18b36
Monica <monica.racero@gmail.com>
2024-05-14 13:44:34 +0200

    ejercicio 1

--- statements.sql
+++ statements.sql
@@ -1,6 +1,6 @@
 -- Ejercicio 1: Seleccionar todos los usuarios y sus lenguajes asociados.
 -- Tu respuesta aquí
-
+SELECT nombre, apellido, lenguaje from usuarios_lenguajes;
 
 -- Ejercicio 2: Contar cuántos usuarios hay en total.
 -- Tu respuesta aquí
